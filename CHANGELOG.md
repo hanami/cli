1f300a7051544604d9df92776d4776de79631189
Luca Guidi <me@lucaguidi.com>
2021-03-11 15:56:36 +0100

    Initial mess

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## [Unreleased]
+
+## [0.1.0] - 2021-03-11
+
+- Initial release
---
+++ spec/hanami/cli_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Hanami::Cli do
+  it "has a version number" do
+    expect(Hanami::Cli::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
