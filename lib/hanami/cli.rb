ec2e6cb85b0a02c34fde0719ceda0832f55812cf
Luca Guidi <me@lucaguidi.com>
2021-03-13 14:41:21 +0100

    Require `"hanami/cli/bundler"` by default

--- lib/hanami/cli.rb
+++ lib/hanami/cli.rb
@@ -6,6 +6,7 @@ module Hanami
   module CLI
     require_relative "cli/version"
     require_relative "cli/error"
+    require_relative "cli/bundler"
     require_relative "cli/commands"
 
     extend Dry::CLI::Registry
