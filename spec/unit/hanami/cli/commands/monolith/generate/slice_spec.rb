782d2bd9622bd439807801b963bf4557021d2675
Luca Guidi <me@lucaguidi.com>
2021-03-18 16:45:07 +0100

    Switch to memory adapter from `Dry::CLI`

--- spec/unit/hanami/cli/commands/monolith/generate/slice_spec.rb
+++ spec/unit/hanami/cli/commands/monolith/generate/slice_spec.rb
@@ -7,7 +7,7 @@ require "ostruct"
 RSpec.describe Hanami::CLI::Commands::Monolith::Generate::Slice do
   subject { described_class.new(fs: fs, inflector: inflector, generator: generator) }
 
-  let(:fs) { RSpec::Support::FileSystem.new }
+  let(:fs) { Dry::CLI::Utils::Files.new(memory: true) }
   let(:inflector) { Dry::Inflector.new }
   let(:generator) { Hanami::CLI::Generators::Slice.new(fs: fs, inflector: inflector) }
   let(:app) { "Bookshelf" }
