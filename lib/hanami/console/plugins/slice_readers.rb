dbc42b543584a709864866fd3c90696e6e6e0662
Tim Riley <tim@openmonkey.com>
2021-12-04 23:34:17 +1100

    Don't delegate to top-level slice components
    This is a neat trick but I think it may lead to more confusion than convenience, especially given that the majority of slice behavior will not be available via these shortcuts, since most slice behavior will not typically reside in top-level components.

--- lib/hanami/console/plugins/slice_readers.rb
+++ lib/hanami/console/plugins/slice_readers.rb
@@ -12,27 +12,7 @@ module Hanami
         def initialize(application)
           application.slices.each do |(name, slice)|
             define_method(name) do
-              SliceDelegator.new(slice)
-            end
-          end
-        end
-
-        # @api private
-        # @since 2.0.0
-        class SliceDelegator < SimpleDelegator
-          # @api private
-          def respond_to_missing?(name)
-            key?(name)
-          end
-
-          private
-
-          # @api private
-          def method_missing(name, *args, &block)
-            if args.empty? && key?(name)
-              self[name]
-            else
-              super
+              slice
             end
           end
         end
