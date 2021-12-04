0c6d100b032a5d612ee8a86bc3680b781b67bef7
Tim Riley <tim@openmonkey.com>
2021-12-04 23:48:10 +1100

    Make slice readers properly available in console
    For clarity, also refactor this module so its methods are built upon initialization rather than when the module is extended (since they already have all the information they need at initialization time).

--- lib/hanami/console/context.rb
+++ lib/hanami/console/context.rb
@@ -10,16 +10,20 @@ module Hanami
     # @since 2.0.0
     class Context < Module
       # @api private
-      def initialize(application)
-        @application = application
-      end
+      attr_reader :application
 
       # @api private
-      def extended(other)
-        super
-        app = @application
+      def initialize(application)
+        @application = application
 
-        extend(Plugins::SliceReaders.new(app))
+        define_context_methods
+        include Plugins::SliceReaders.new(application)
+      end
+
+      private
+
+      def define_context_methods
+        app = application
 
         define_method(:inspect) do
           "#<#{self.class} application=#{app} env=#{app.config.env}>"
