1a152a1d5b283ba59ed70a4cdbe4ae840776f714
Tim Riley <tim@openmonkey.com>
2022-03-11 08:33:15 +1100

    Prepare for v2.0.0.alpha7

--- lib/hanami/cli/version.rb
+++ lib/hanami/cli/version.rb
@@ -2,6 +2,6 @@
 
 module Hanami
   module CLI
-    VERSION = "2.0.0.alpha6.1"
+    VERSION = "2.0.0.alpha7"
   end
 end
--- spec/unit/hanami/cli/version_spec.rb
+++ spec/unit/hanami/cli/version_spec.rb
@@ -2,6 +2,6 @@
 
 RSpec.describe "Hanami::CLI::VERSION" do
   it "returns version" do
-    expect(Hanami::CLI::VERSION).to eq("2.0.0.alpha6.1")
+    expect(Hanami::CLI::VERSION).to eq("2.0.0.alpha7")
   end
 end
