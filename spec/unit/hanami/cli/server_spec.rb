b3cc93f8212dc856c218e33594f814ce9a7b0c45
Marc Busqué <marc@lamarciana.com>
2022-06-29 10:37:07 +0200

    Add `hanami server` command (#22)
    The server is just a thin wrapper on top of Rack::Server. The options
    that it accepts fall into two different categories:
    
    - When not explicitly set, port and host are not passed to the rack
    server instance. This way, they can be configured through the configured
    rack handler (e.g., the puma configuration file).
    
    - All others are always given by the Hanami command.
    
    The test for the command forks a new process so that the one running the
    tests is not blocked and can assert that endpoint is working. Standard
    outputs are silenced to avoid the command output being rendered.

---
+++ spec/unit/hanami/cli/server_spec.rb
@@ -0,0 +1,47 @@
+require "hanami/cli/server"
+
+RSpec.describe Hanami::CLI::Server do
+  describe "#call" do
+    subject { described_class.new(rack_server: rack_server) }
+
+    let(:rack_server) do
+      Class.new do
+        attr_reader :options
+
+        def start(options)
+          @options = options
+        end
+      end.new
+    end
+
+    it "delegates host as Host" do
+      subject.call(host: "127.0.0.3")
+
+      expect(subject.rack_server.options[:Host]).to eq("127.0.0.3")
+    end
+
+    it "delegates port as Port" do
+      subject.call(port: 2000)
+
+      expect(subject.rack_server.options[:Port]).to be(2000)
+    end
+
+    it "delegates config as config" do
+      subject.call(config: "config.ru")
+
+      expect(subject.rack_server.options[:config]).to eq("config.ru")
+    end
+
+    it "delegates debug as debug" do
+      subject.call(debug: true)
+
+      expect(subject.rack_server.options[:debug]).to be(true)
+    end
+
+    it "delegates warn as warn" do
+      subject.call(warn: true)
+
+      expect(subject.rack_server.options[:warn]).to be(true)
+    end
+  end
+end
