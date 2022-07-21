b789b37d61678d43f26887f2e042d964dae697ac
Tim Riley <tim@riley.id.au>
2022-07-21 14:04:02 +1000

    Put hanami gem first lin list
    Feels better this way, since this line will never be removed

--- spec/unit/hanami/cli/commands/gem/new_spec.rb
+++ spec/unit/hanami/cli/commands/gem/new_spec.rb
@@ -62,10 +62,10 @@ RSpec.describe Hanami::CLI::Commands::Gem::New do
 
         gem "rake"
 
+        gem "hanami", "#{hanami_version}"
         gem "hanami-router", "#{hanami_version}"
         gem "hanami-controller", "#{hanami_version}"
         gem "hanami-validations", "#{hanami_version}"
-        gem "hanami", "#{hanami_version}"
 
         gem "puma"
 
