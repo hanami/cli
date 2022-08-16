bfdfac166ff9e657cecd9dff3387a5bfa74c7848
Tim Riley <tim@riley.id.au>
2022-08-16 12:07:06 +1000

    Deindent these
    It’s going to be annoying to have these shift every time a new command is introduced

--- lib/hanami/cli/commands/app.rb
+++ lib/hanami/cli/commands/app.rb
@@ -25,12 +25,12 @@ module Hanami
 
         def self.extended(base)
           base.module_eval do
-            register "version",      Commands::App::Version, aliases: ["v", "-v", "--version"]
-            register "install",      Commands::App::Install
-            register "console",      Commands::App::Console, aliases: ["c"]
-            register "server",       Commands::App::Server,  aliases: ["s"]
-            register "routes",       Commands::App::Routes
-            register "middlewares",  Commands::App::Middlewares
+            register "version", Commands::App::Version, aliases: ["v", "-v", "--version"]
+            register "install", Commands::App::Install
+            register "console", Commands::App::Console, aliases: ["c"]
+            register "server", Commands::App::Server, aliases: ["s"]
+            register "routes", Commands::App::Routes
+            register "middlewares", Commands::App::Middlewares
 
             register "generate", aliases: ["g"] do |prefix|
               prefix.register "slice", Generate::Slice
