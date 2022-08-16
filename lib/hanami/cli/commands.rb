34e22191c9ec706fc5a5844f8451e437ddbd8150
Tim Riley <tim@riley.id.au>
2022-08-16 21:42:50 +1000

    Restore .within_hanami_app?
    Turns out this is used by hanami-rspec’s CLI command extensions.

--- lib/hanami/cli/commands.rb
+++ lib/hanami/cli/commands.rb
@@ -4,10 +4,22 @@ require "hanami"
 
 module Hanami
   module CLI
+    # Returns true if the CLI is being called from inside an Hanami app.
+    #
+    # This is typically used to determine whether to register commands that are applicable either
+    # inside or outside an app.
+    #
+    # @api public
+    # @since 2.0.0
+    def self.within_hanami_app?
+      !!Hanami.app_path
+    end
+
     module Commands
     end
 
-    def self.register_commands!(within_hanami_app = Hanami.app_path)
+    # @api private
+    def self.register_commands!(within_hanami_app = within_hanami_app?)
       commands = if within_hanami_app
                    require_relative "commands/app"
                    Commands::App
