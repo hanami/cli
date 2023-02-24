cd036d5ccc97c5eac80f7bbf739af3c622f8f8ba
Charith Perera <charith.p@gmail.com>
2023-02-24 14:58:23 +1100

    Fix db rollback command
    The current implementation of the rollback command does not work. The
    target that it passes to the run_migrations method of the database util
    class is the current migration which results in a no-op.
    
    This change addresses this problem by explicitly targeting the second
    last applied migration. This change also refactors the logic for
    when no migrations exist to rollback to provide a clearer message.

--- spec/unit/hanami/cli/commands/app/db/rollback_spec.rb
+++ spec/unit/hanami/cli/commands/app/db/rollback_spec.rb
@@ -1,26 +1,78 @@
 # frozen_string_literal: true
 
 RSpec.describe Hanami::CLI::Commands::App::DB::Rollback, :app, :command, :db do
-  it "rolls back to specified migration" do
-    expect(database).to receive(:applied_migrations).and_return(["312_create_users"])
+  context "given no target is specified" do
+    context "there a no previous migrations" do
+      it "rolls back to one before the current migration" do
+        expect(database).to receive(:applied_migrations).and_return([])
+        expect(database).to_not receive(:run_migrations)
 
-    if RUBY_VERSION > "3.2"
-      expect(database).to receive(:run_migrations).with({target: 312}).and_return(true)
-    else
-      expect(database).to receive(:run_migrations).with(target: 312).and_return(true)
+        command.call(dump: false)
+
+        expect(output).to include("no migrations to rollback")
+      end
     end
 
-    command.call(target: "312", dump: false)
+    context "there is one previous migration" do
+      it "rolls back to the initial state" do
+        expect(database).to receive(:applied_migrations).and_return(["312_create_users"])
 
-    expect(output).to include("database test rolled back to 312_create_users")
+        if RUBY_VERSION > "3.2"
+          expect(database).to receive(:run_migrations).with({target: 311}).and_return(true)
+        else
+          expect(database).to receive(:run_migrations).with(target: 311).and_return(true)
+        end
+
+        command.call(dump: false)
+
+        expect(output).to include("database test rolled back to initial state")
+      end
+    end
+
+    context "there are multiple previous migrations" do
+      it "rolls back to one before the current migration" do
+        expect(database).to receive(:applied_migrations).and_return(
+          [
+            "312_create_users",
+            "313_create_books",
+          ]
+        )
+
+        if RUBY_VERSION > "3.2"
+          expect(database).to receive(:run_migrations).with({target: 312}).and_return(true)
+        else
+          expect(database).to receive(:run_migrations).with(target: 312).and_return(true)
+        end
+
+        command.call(dump: false)
+
+        expect(output).to include("database test rolled back to 312_create_users")
+      end
+    end
   end
 
-  it "warns if target migration is not found" do
-    expect(database).to receive(:applied_migrations).and_return([])
-    expect(database).to_not receive(:run_migrations)
+  context "given a target is specified" do
+    it "rolls back to specified migration" do
+      expect(database).to receive(:applied_migrations).and_return(["312_create_users"])
 
-    command.call(target: "312", dump: false)
+      if RUBY_VERSION > "3.2"
+        expect(database).to receive(:run_migrations).with({target: 312}).and_return(true)
+      else
+        expect(database).to receive(:run_migrations).with(target: 312).and_return(true)
+      end
 
-    expect(output).to include("migration file for target 312 was not found")
+      command.call(target: "312", dump: false)
+
+      expect(output).to include("database test rolled back to 312_create_users")
+    end
+
+    it "warns if target migration is not found" do
+      expect(database).to receive(:applied_migrations).and_return([])
+      expect(database).to_not receive(:run_migrations)
+
+      command.call(target: "312", dump: false)
+
+      expect(output).to include("migration file for target 312 was not found")
+    end
   end
 end
