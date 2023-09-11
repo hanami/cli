0e8d0c2ab68c4a1f556ed6d381ea29d034181107
Luca Guidi <me@lucaguidi.com>
2023-09-11 10:32:03 +0200

    Generate `Gemfile` with `hanami-assets` gem

--- spec/unit/hanami/cli/commands/gem/new_spec.rb
+++ spec/unit/hanami/cli/commands/gem/new_spec.rb
@@ -92,6 +92,7 @@ RSpec.describe Hanami::CLI::Commands::Gem::New do
         gem "hanami-controller", "#{hanami_version}"
         gem "hanami-validations", "#{hanami_version}"
         gem "hanami-view", "#{hanami_version}"
+        gem "hanami-assets", "#{hanami_version}"
         gem "hanami-webconsole", "#{hanami_version}"
 
         gem "dry-types", "~> 1.0", ">= 1.6.1"
