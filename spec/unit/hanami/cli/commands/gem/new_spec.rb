abbb927ae1dbc2aaede98d78adf43910d86aebd3
Luca Guidi <me@lucaguidi.com>
2023-09-13 15:36:16 +0200

    Remove cargo-culted meta tag

--- spec/unit/hanami/cli/commands/gem/new_spec.rb
+++ spec/unit/hanami/cli/commands/gem/new_spec.rb
@@ -275,7 +275,6 @@ RSpec.describe Hanami::CLI::Commands::Gem::New do
           <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <meta http-equiv="X-UA-Compatible" content="ie=edge">
             <title>#{inflector.humanize(app)}</title>
             <%= favicon %>
             <%= stylesheet "app" %>
