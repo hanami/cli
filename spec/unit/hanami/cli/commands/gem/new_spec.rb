e9a769aeb27254abb9c55cb6ab43ee4600e3d571
Luca Guidi <me@lucaguidi.com>
2023-10-06 16:06:09 +0200

    `hanami new` to generate documented `config/puma.rb` (#108)
    * `hanami new` to generate documented `config/puma.rb`
    
    * Remove full stops from end of headings
    
    This makes consistent and more headling-like.
    
    * Ensure port is a real integer
    
    This will ensure we don’t accept invalid input from the ENV var
    
    * Use puma as the prefix for concurrenc/cluster vars
    
    This makes it clearer they’re controlling Puma-level concerns only, and are not dealing with anything specific to Hanami at a framework-level.
    
    * Ensure puma_concurrency is a real integer
    
    Without this, a value coming in from the ENV will be strings and we can’t call integer methods on it afterwards (which we actually do here).
    
    * Only activate puma cluster mode for concurrency > 1
    
    Cluster mode isn’t needed when the concurrency is exactly 1, and Puma will in fact print warnings if this is activated in this way.
    
    * Run Hanami.shutdown in Puma before_fork block
    
    This matches what Sequel recommends in its Fork Safety document (http://sequel.jeremyevans.net/rdoc/files/doc/fork_safety_rdoc.html), and given Sequel-managed database connections (via rom-rb) are soon to be part of Hanami, aligning with Sequel’s advice here now makes sense.
    
    In addition, this matches Heroku’s concurrency and database connections advice when using multi-process web servers (https://devcenter.heroku.com/articles/concurrency-and-database-connections#multi-process-servers).
    
    * Remove unneeded Integer cast
    
    ---------
    
    Co-authored-by: Tim Riley <tim@riley.id.au>

--- spec/unit/hanami/cli/commands/gem/new_spec.rb
+++ spec/unit/hanami/cli/commands/gem/new_spec.rb
@@ -197,21 +197,51 @@ RSpec.describe Hanami::CLI::Commands::Gem::New do
       puma = <<~EXPECTED
         # frozen_string_literal: true
 
+        #
+        # Environment and port
+        #
+        port ENV.fetch("HANAMI_PORT", 2300)
+        environment ENV.fetch("HANAMI_ENV", "development")
+
+        #
+        # Threads within each Puma/Ruby process (aka worker)
+        #
+
+        # Configure the minimum and maximum number of threads to use to answer requests.
         max_threads_count = ENV.fetch("HANAMI_MAX_THREADS", 5)
         min_threads_count = ENV.fetch("HANAMI_MIN_THREADS") { max_threads_count }
+
         threads min_threads_count, max_threads_count
 
-        port        ENV.fetch("HANAMI_PORT", 2300)
-        environment ENV.fetch("HANAMI_ENV", "development")
-        workers     ENV.fetch("HANAMI_WEB_CONCURRENCY", 0)
+        #
+        # Workers (aka Puma/Ruby processes)
+        #
 
-        if ENV.fetch("HANAMI_WEB_CONCURRENCY", 0) > 0
-          on_worker_boot do
+        puma_concurrency = Integer(ENV.fetch("HANAMI_WEB_CONCURRENCY", 0))
+        puma_cluster_mode = puma_concurrency > 1
+
+        # How many worker (Puma/Ruby) processes to run.
+        # Typically this is set to the number of available cores.
+        workers puma_concurrency
+
+        #
+        # Cluster mode (aka multiple workers)
+        #
+
+        if puma_cluster_mode
+          # Preload the application before starting the workers. Only in cluster mode.
+          preload_app!
+
+          # Code to run immediately before master process forks workers (once on boot).
+          #
+          # These hooks can block if necessary to wait for background operations unknown
+          # to puma to finish before the process terminates. This can be used to close
+          # any connections to remote servers (database, redis, …) that were opened when
+          # preloading the code.
+          before_fork do
             Hanami.shutdown
           end
         end
-
-        preload_app!
       EXPECTED
       expect(fs.read("config/puma.rb")).to eq(puma)
       expect(output).to include("Created config/puma.rb")
