028eac19f3cff2583009282989ab5c335216e175
Tim Riley <tim@riley.id.au>
2024-06-16 15:38:34 +1000

    Append schema_migration insert to structure dump
    This will make it possible to cleanly run migrations after loading the structure.

--- lib/hanami/cli/commands/app/db/structure/dump.rb
+++ lib/hanami/cli/commands/app/db/structure/dump.rb
@@ -13,18 +13,33 @@ module Hanami
 
               # @api private
               def call(app: false, slice: nil, **)
-                databases(app: app, slice: slice).each do |database|
-                  slice_root = database.slice.root.relative_path_from(database.slice.app.root)
-                  structure_path = slice_root.join("config", "db", "structure.sql")
+                exit_codes = []
 
-                  measure("#{database.name} structure dumped to #{structure_path}") do
-                    database.exec_dump_command.tap do |result|
+                databases(app: app, slice: slice).each do |database|
+                  structure_path = database.slice.root.join("config", "db", "structure.sql")
+                  relative_structure_path = structure_path.relative_path_from(database.slice.app.root)
+
+                  measure("#{database.name} structure dumped to #{relative_structure_path}") do
+                    catch :dump_failed do
+                      result = database.exec_dump_command
+                      exit_codes << result.exit_code if result.respond_to?(:exit_code)
+
                       unless result.successful?
                         out.puts result.err
-                        break false
+                        throw :dump_failed, false
                       end
+
+                      File.open(structure_path, "a") do |f|
+                        f.puts "#{database.schema_migrations_sql_dump}\n"
+                      end
+
+                      true
                     end
                   end
+                end
+
+                exit_codes.each do |code|
+                  break exit code if code > 0
                 end
               end
             end
