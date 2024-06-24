6f6fce30674587d9ccfad0fce1d5eca294cf26e7
Sean Collins <sean@cllns.com>
2024-06-24 14:21:26 -0600

    Remove last temporary instance variable

--- lib/hanami/cli/generators/app/operation.rb
+++ lib/hanami/cli/generators/app/operation.rb
@@ -23,8 +23,8 @@ module Hanami
           # @since 2.2.0
           # @api private
           def call(app_namespace, key, slice)
-            @camelized_app_name = inflector.camelize(app_namespace).gsub(/[^\p{Alnum}]/, "")
-            @key = key
+            camelized_app_name = inflector.camelize(app_namespace).gsub(/[^\p{Alnum}]/, "")
+            operation_name = key.split(KEY_SEPARATOR)[-1]
             namespaces = key.split(KEY_SEPARATOR)[..-2]
 
             if slice
@@ -32,7 +32,8 @@ module Hanami
               raise MissingSliceError.new(slice) unless fs.directory?(slice_directory)
             end
 
-            fs.mkdir(directory(slice, namespaces: namespaces))
+            directory = directory(slice, namespaces: namespaces)
+            fs.mkdir(directory)
 
             if namespaces.none?
               out.puts(
@@ -41,30 +42,24 @@ module Hanami
               )
             end
 
-            path = fs.join(directory(slice, namespaces: namespaces), "#{operation_name}.rb")
+            highest_level_module =
+              if slice
+                inflector.camelize(slice).gsub(/[^\p{Alnum}]/, "")
+              else
+                camelized_app_name
+              end
 
-            fs.write(path, class_definition(slice, namespaces: namespaces))
+            path = fs.join(directory, "#{operation_name}.rb")
+
+            file_contents = class_definition(highest_level_module: highest_level_module,
+                                             operation_name: operation_name, namespaces: namespaces)
+
+            fs.write(path, file_contents)
           end
 
           private
 
           attr_reader :fs, :inflector, :out
-
-          def operation_name
-            @key.split(KEY_SEPARATOR)[-1]
-          end
-
-          def camelized_operation_name
-            inflector.camelize(operation_name).gsub(/[^\p{Alnum}]/, "")
-          end
-
-          def highest_level_module(slice)
-            if slice
-              inflector.camelize(slice).gsub(/[^\p{Alnum}]/, "")
-            else
-              @camelized_app_name
-            end
-          end
 
           def directory(slice = nil, namespaces:)
             base = if slice
@@ -80,14 +75,16 @@ module Hanami
             end
           end
 
-          def class_definition(slice, namespaces:)
+          def class_definition(highest_level_module:, operation_name:, namespaces:)
+            camelized_operation_name = inflector.camelize(operation_name).gsub(/[^\p{Alnum}]/, "")
+
             camelized_modules = if namespaces.any?
-                                  [highest_level_module(slice)].push(namespaces.map { inflector.camelize(_1) }).flatten
+                                  [highest_level_module].push(namespaces.map { inflector.camelize(_1) }).flatten
                                 else
-                                  [highest_level_module(slice)]
+                                  [highest_level_module]
                                 end
 
-            parent_class = [highest_level_module(slice), "Operation"].join("::")
+            parent_class = [highest_level_module, "Operation"].join("::")
 
             RubyFileGenerator.class(
               camelized_operation_name,
