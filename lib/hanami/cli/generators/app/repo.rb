20f2407017c235cd03dbf119cc39b00833d9fc77
Sean Collins <cllns@users.noreply.github.com>
2024-07-13 20:33:47 -0600

    Generate repo (#200)
    * Add dry-operation to default Gemfile
    
    * Add base Operation class, based on dry-operation
    
    * Fix view spec
    
    * Add Operation generators
    
    * Add empty `call` method definition
    
    * Remove ostruct
    
    * Allow slash separator for generator
    
    * Allow slash separator for generator
    
    * Rename module to admin
    
    * Remove newlines in generated files
    
    By adding new templates for un-nested operations
    
    * Remove input as default args
    
    * Remove Operations namespace, generate in app/ or slices/SLICE_NAME/
    
    * Prevent generating operation without namespace
    
    * Revert "Prevent generating operation without namespace"
    
    This reverts commit a5bd2f30311293c24594d4c84bb0c1033dd055e3.
    
    * Add recommendation to add namespace to operations
    
    * Change examples
    
    * Switch to outputting directly, remove Files#recommend
    
    * Add Hanami::CLI::RubyFileGenerator
    
    * x.x.x => 2.2.0
    
    * x.x.x => 2.2.0
    
    * Include Dry::Monads[:result] in base Action
    
    * Add .module tests
    
    * Convert top-level app operation to use RubyFileGenerator
    
    * Convert nested app operation to use RubyFileGenerator
    
    * Support slash separators
    
    * Convert top-level slice operation to use RubyFileGenerator
    
    * Remove OperationContext
    
    * Remove namespaces instance variable
    
    * Refactor to variables
    
    * Remove last temporary instance variable
    
    * Refactor
    
    * More refactoring, for clarity
    
    * Rename variable for clarity
    
    * Rename helper method
    
    * Simplify RubyFileGenerator, support older versions
    
    * Convert Operation generator to use simplified RubyFileGenerator
    
    * Remove un-used errors
    
    * Refactor
    
    * Older kwargs forwarding style
    
    * Refactor
    
    * Rename variable
    
    * Add explanatory comment
    
    Add dry-monads include for slice base action
    
    * Fix base slice action
    
    * Remove un-used ERB templates
    
    * Remove OperationContext
    
    * Ternary over and/or
    
    * Fix missing 'end' from bad merge
    
    * Fix namespace recommendation
    
    * Add struct generator
    
    * Extract App::Generate::Command
    
    * Extract App::Generate::Command
    
    * Specify full name, to use App::Command
    
    * Specify full name, to use App::Command
    
    * Use App::Generate::Command for Struct
    
    * Use KEY_SEPARATOR from constants file
    
    * Use constants file
    
    * Provide generator class to command
    
    * Extract Helper, use for Struct generator
    
    * Fix specs with Helper
    
    * kwargs
    
    * Rename helper to RubyFileWriter
    
    * Fix examples
    
    * Remove optional kwarg
    
    * Reorder args
    
    * Rename to relative_parent_class
    
    * Add repo generator
    
    * Remove duplicate implementation
    
    * Add api doc comments
    
    * Reorder methods
    
    * Refactor initialize
    
    * Refactor to use method instead of arg
    
    * Refactor to move logic into generator
    
    * Reorder assignments
    
    * Adapt to new API for RubyFileWriter
    
    * Use super
    
    * Remove space
    
    * Name kwargs for Ruby 3.1
    
    * Switch to singularize from pluralize
    
    * Remove shellescape

---
+++ lib/hanami/cli/generators/app/repo.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require "erb"
+require "dry/files"
+require_relative "../constants"
+require_relative "../../errors"
+
+module Hanami
+  module CLI
+    module Generators
+      module App
+        # @since 2.2.0
+        # @api private
+        class Repo
+          # @since 2.2.0
+          # @api private
+          def initialize(fs:, inflector:, out: $stdout)
+            @fs = fs
+            @inflector = inflector
+            @out = out
+          end
+
+          # @since 2.2.0
+          # @api private
+          def call(app_namespace, key, slice)
+            RubyFileWriter.new(
+              fs: fs,
+              inflector: inflector,
+              app_namespace: app_namespace,
+              key: key,
+              slice: slice,
+              extra_namespace: "Repos",
+              relative_parent_class: "DB::Repo",
+              body: [],
+            ).call
+          end
+
+          private
+
+          attr_reader :fs, :inflector, :out
+        end
+      end
+    end
+  end
+end
