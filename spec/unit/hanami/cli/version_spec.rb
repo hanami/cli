8b48ea139f46bca8fc23ee1f73e17b595111db16
Tim Riley <tim@riley.id.au>
2024-09-25 20:53:17 +1000

    Prepare for v2.2.0.beta2

--- spec/unit/hanami/cli/version_spec.rb
+++ spec/unit/hanami/cli/version_spec.rb
@@ -2,6 +2,6 @@
 
 RSpec.describe "Hanami::CLI::VERSION" do
   it "returns version" do
-    expect(Hanami::CLI::VERSION).to eq("2.2.0.beta1")
+    expect(Hanami::CLI::VERSION).to eq("2.2.0.beta2")
   end
 end
