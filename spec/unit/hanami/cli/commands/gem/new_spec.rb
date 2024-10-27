dfa93c9cd235dbc3b57161148b50f9ede18c1f5a
Adam Lassek <adam@doubleprime.net>
2024-10-27 17:37:32 -0500

    Remove ROM boilerplate from Operation generator (#240)
    Closes #1437
    
    This is now being handled by the framework automatically in
    hanami/hanami#1456

--- spec/unit/hanami/cli/commands/gem/new_spec.rb
+++ spec/unit/hanami/cli/commands/gem/new_spec.rb
@@ -1020,7 +1020,6 @@ RSpec.describe Hanami::CLI::Commands::Gem::New do
         expect(fs.exist?("app/db/struct.rb")).to be(false)
         expect(fs.exist?("app/db/relation.rb")).to be(false)
         expect(fs.exist?("config/db/")).to be(false)
-        expect(fs.read("app/operation.rb")).to_not match("ROM")
         expect(fs.read(".gitignore")).to_not include("db/*.sqlite")
         expect(fs.exist?("db/")).to be(false)
       end
