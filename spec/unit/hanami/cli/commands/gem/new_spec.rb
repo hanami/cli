6f6316e2d068de2e5b674d2f5a393f20e6adcddc
Tim Riley <tim@riley.id.au>
2024-10-28 22:26:10 +1100

    Improve seeds loading (#256)
    Improve the comments in the seeds file.
    
    Print notices if `db seed` is run but expected seeds files are not found.
    
    Co-authored-by: François Beausoleil <francois@inspectify.com>
    Co-authored-by: Tim Riley <tim@riley.id.au>

--- spec/unit/hanami/cli/commands/gem/new_spec.rb
+++ spec/unit/hanami/cli/commands/gem/new_spec.rb
@@ -949,13 +949,18 @@ RSpec.describe Hanami::CLI::Commands::Gem::New do
         expect(output).to include("Created app/structs/.keep")
 
         seeds = <<~EXPECTED
-          # This seeds file should ensure the existence of database records required to run the app.
+          # This seeds file should create the database records required to run the app.
           #
           # The code should be idempotent so that it can be executed at any time.
           #
           # To load the seeds, run `hanami db seed`. Seeds are also loaded as part of `hanami db prepare`.
 
-          # Example:
+          # For example, if you have appropriate repos available:
+          #
+          #   category_repo = Hanami.app["repos.category_repo"]
+          #   category_repo.create(title: "General")
+          #
+          # Alternatively, you can use relations directly:
           #
           #   categories = Hanami.app["relations.categories"]
           #   categories.insert(title: "General")
