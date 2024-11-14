8ee5f6de23e78a286dbd2a9b0bef2d8dcb30621b
Hana Rimawi <hana.rimawi@tastytrade.com>
2024-11-14 15:20:39 -0600

    Provide helpful instructions in new app README (#273)

--- spec/unit/hanami/cli/commands/gem/new_spec.rb
+++ spec/unit/hanami/cli/commands/gem/new_spec.rb
@@ -126,6 +126,20 @@ RSpec.describe Hanami::CLI::Commands::Gem::New do
       # README.md
       readme = <<~EXPECTED
         # #{inflector.camelize(app)}
+
+        Welcome to your Hanami app!
+
+        ## Getting Started
+
+        - Run the server with `bin/dev`
+        - View the app at [http://localhost:2300](http://localhost:2300)
+        - Run the tests with `bundle exec rake`
+
+        ## Useful Links
+
+        - [Hanami Home](http://hanamirb.org)
+        - [Hanami Guides](https://guides.hanamirb.org/)
+        - [Hanami API Doc](https://gemdocs.org/gems/hanami/latest)
       EXPECTED
       expect(fs.read("README.md")).to eq(readme)
       expect(output).to include("Created README.md")
@@ -590,6 +604,20 @@ RSpec.describe Hanami::CLI::Commands::Gem::New do
         # README.md
         readme = <<~EXPECTED
           # #{inflector.camelize(app)}
+
+          Welcome to your Hanami app!
+  
+          ## Getting Started
+  
+          - Run the server with `bin/dev`
+          - View the app at [http://localhost:2300](http://localhost:2300)
+          - Run the tests with `bundle exec rake`
+  
+          ## Useful Links
+  
+          - [Hanami Home](http://hanamirb.org)
+          - [Hanami Guides](https://guides.hanamirb.org/)
+          - [Hanami API Doc](https://gemdocs.org/gems/hanami/latest)
         EXPECTED
         expect(fs.read("README.md")).to eq(readme)
         expect(output).to include("Created README.md")
@@ -1125,6 +1153,20 @@ RSpec.describe Hanami::CLI::Commands::Gem::New do
       # README.md
       readme = <<~EXPECTED
         # #{inflector.camelize(app)}
+
+        Welcome to your Hanami app!
+
+        ## Getting Started
+
+        - Run the server with `bin/dev`
+        - View the app at [http://localhost:2300](http://localhost:2300)
+        - Run the tests with `bundle exec rake`
+
+        ## Useful Links
+
+        - [Hanami Home](http://hanamirb.org)
+        - [Hanami Guides](https://guides.hanamirb.org/)
+        - [Hanami API Doc](https://gemdocs.org/gems/hanami/latest)
       EXPECTED
       expect(fs.read("README.md")).to eq(readme)
 
