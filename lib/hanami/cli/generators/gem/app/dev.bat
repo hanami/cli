d65ab1a03cc24d736494136fa541ca230b6070e7
Kyle Plump <kplump1395@gmail.com>
2024-11-27 11:56:01 -0500

    added correct dev.bat file for windows platform
    Co-authored-by: dsisnero <dsisnero@gmail.com>

--- lib/hanami/cli/generators/gem/app/dev.bat
+++ lib/hanami/cli/generators/gem/app/dev.bat
@@ -1,1 +1,11 @@
-@bash.exe "%~dpn0" %*
+@echo off
+  gem list -i foreman --silent && (
+        echo Foreman is installed
+        foreman start -f Procfile.dev %*
+    ) || (
+    echo Foreman not installed. Attempting installation...
+    call gem install foreman && (
+        echo Foreman is installed
+        foreman start -f Procfile.dev %*
+        )
+)
