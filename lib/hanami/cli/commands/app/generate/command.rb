31d224355d1bd6d59b8c8be23d2e6295aad7d140
mrimp <krzysiekkamilpiotrek@gmail.com>
2025-08-04 11:36:23 +0200

    cleanup

--- lib/hanami/cli/commands/app/generate/command.rb
+++ lib/hanami/cli/commands/app/generate/command.rb
@@ -37,21 +37,6 @@ module Hanami
             def detect_slice_from_cwd
               slices_by_root = app.slices.with_nested.each.to_h { |slice| [slice.root.to_s, slice] }
               slices_by_root[fs.pwd]
-              # # This has to be Pathname.pwd I think, otherwise we don't know how deeply we are nested
-              # # https://github.com/search?q=repo%3Adry-rb%2Fdry-files%20path%3A%2F%5Espec%5C%2Funit%5C%2Fdry%5C%2Ffiles%5C%2F%2F%20pwd&type=code
-              # # unless this links shows me I am wrong
-              # current_dir = Dir.pwd
-              # slices_dir = fs.join(app.root.to_s, "slices")
-              # return unless current_dir.start_with?(slices_dir)
-              #
-              # relative_path = current_dir.delete_prefix("#{slices_dir}/")
-              # slice_name = relative_path.split("/").first
-              # return unless app.slices.keys.include?(slice_name.to_sym)
-              #
-              # slices_by_root = app.slices.with_nested.each.to_h { |slice| [slice.root, slice] }
-              # slices_by_root[current_dir]
-              # # slice_name if slices_by_root[current_dir.to_sym]
-              # slice_name if slices_by_root[current_dir.to_sym]
             end
 
             # @since 2.2.0
